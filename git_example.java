94bbe05bc2eac3d126d59de50df22aac99cca1b3
KwonHyeongChan <gudcks93@gmail.com>
2024-11-25 13:43:15 +0900

    add complete

--- git_example.java
+++ git_example.java
@@ -1,6 +1,13 @@
 class Test{
   public static void main(String[] args)
   {
+
     System.out.println("this is master.");
+  }
+  int add(int a, int b){
+    int result;
+    result = a+b;
+
+    return result;
   }
 }
