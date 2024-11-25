e7d90440cf558e777d2c69799c0eb14810d3fad9
koseoyeon <woogwayeon@gmail.com>
2024-11-25 13:37:52 +0900

    create minus function + use main()

--- git_example.java
+++ git_example.java
@@ -3,5 +3,10 @@ class Test{
   {
     System.out.println("this is master.");
     System.out.println("this is yeon.");
+
+    minus();
+    // minus 함수 호출 완료 - 2024-11-25 seoyeon
   }
+
+  static void minus(){}
 }
